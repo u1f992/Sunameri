91b40a1a47d2e1276e6482e9569e814538f92fab
Kôtaro Mukai <mukai.k1011k@outlook.jp>
2022-05-22 21:41:35 +0900

    sample

--- sample/xd-adjust-seed.js
+++ sample/xd-adjust-seed.js
@@ -29,36 +29,106 @@ function enableVibration(controller, sequences) {
 }
 
 /**
+ * @typedef {{ pivotSeed: System.UInt32, targetSeed: System.UInt32, waitTime: System.TimeSpan }} SelectedResult
+ */
+/**
  * 初期seed厳選
  * 
  * 事前条件: XDが起動しており、ソフトリセットできる。
  * 
  * 事後条件: 現在のseedが判明し、いますぐバトルの「はい」にカーソルが合っている。
  * 
- * @param {any} targetSeeds 
+ * @param {number[]} targetSeeds 
  * @param {Sunameri.SerialPortWrapper} controller 
  * @param {Sunameri.VideoCaptureWrapper} capture 
  * @param {Sequences} sequences 
+ * @param {Sunameri.TesseractConfig} tessConfig 
+ * @param {QuickBattleDetectInformation} detectInfo 
+ * @param {TrainerInformation} trainerInfo 
+ * @param {AdvanceInformation} advanceInfo 
+ * @param {WaitTimeInformation} waitTimeInfo 
+ * @returns {SelectedResult}
  */
-function selectSeed(targetSeeds, controller, capture, sequences, tessConfig, detectInfo, trainerInfo) {
+function selectSeed(targetSeeds, controller, capture, sequences, tessConfig, detectInfo, trainerInfo, advanceInfo, waitTimeInfo) {
 
-    // 1回目捨てまで
-    controller.run([
-        [].concat(
-            sequences.reset,
-            sequences.moveQuickBattle,
-            sequences.loadParties,
-            sequences.discardParties
-        )
-    ]);
+    /** @type {System.UInt32} */
+    let currentSeed;
 
-    const currentSeed = getCurrentSeed(controller, capture, sequences, tessConfig, detectInfo, trainerInfo);
-    System.Console.WriteLine(currentSeed);
+    do {
+        // 1回目捨てまで
+        controller.run([
+            [].concat(
+                sequences.reset,
+                sequences.moveQuickBattle,
+                sequences.loadParties,
+                sequences.discardParties
+            )
+        ]);
+
+        // getCurrentSeedが失敗したらリセット
+        // = 5回連続でgetQuickBattleDataが失敗
+        let flag = false;
+        try {
+            currentSeed = getCurrentSeed(controller, capture, sequences, tessConfig, detectInfo, trainerInfo);
+        } catch (error) {
+            System.Console.Error.WriteLine(error.message);
+            flag = true;
+        }
+        if (flag) {
+            continue;
+        }
+        
+        for (let i = 0; i < targetSeeds.length; i++) {
+
+            const targetSeed = targetSeeds[i].toUInt32();
+            const waitTime = getWaitTime(currentSeed, targetSeed, advanceInfo);
+
+            if (isCloseEnough(waitTime, waitTimeInfo)) {
+                return {
+                    pivotSeed: currentSeed,
+                    targetSeed: targetSeed,
+                    waitTime: waitTime
+                };
+            }
+        }
+    } while (true);
+}
+
+/**
+ * @typedef {{ faster: {perSecond: number}, afterLoad?: {byLoading: number, byOpeningItems: number} }} AdvanceInformation
+ * @typedef {{ maximum: System.TimeSpan, minimum: System.TimeSpan }} WaitTimeInformation
+ */
+/**
+ * waitTimeがminimum以上maximum以下
+ * 
+ * @param {System.TimeSpan} waitTime
+ * @param {WaitTimeInformation} waitTimeInfo 
+ * @returns {boolean}
+ */
+function isCloseEnough(waitTime, waitTimeInfo) {
+
+    // C#側の演算子のオーバーロードはClearScript側から利用できない
+    const longerThanMinimum = waitTime.CompareTo(waitTimeInfo.minimum) >= 0;
+    const shorterThanMaximum = waitTime.CompareTo(waitTimeInfo.maximum) <= 0;
+    
+    return longerThanMinimum && shorterThanMaximum;
+}
+
+/**
+ * advanceInfoに基づいてseed間の待機時間を算出する
+ * 
+ * @param {System.UInt32} currentSeed 
+ * @param {System.UInt32} targetSeed 
+ * @param {AdvanceInformation} advanceInfo 
+ * @returns {System.TimeSpan}
+ */
+function getWaitTime(currentSeed, targetSeed, advanceInfo) {
+    return System.TimeSpan.FromSeconds(GCLCGExtension.GetIndex(targetSeed, currentSeed) / advanceInfo.faster.perSecond);
 }
 
 /**
  * @typedef {{ player: {icon: {rect: Sunameri.Rect, template: Sunameri.Mat[]}, hp: Sunameri.Rect[]}, com: {icon: {rect: Sunameri.Rect, template: Sunameri.Mat[]}, hp: Sunameri.Rect[]} }} QuickBattleDetectInformation
- * @typedef {{ tsv: any }} TrainerInformation
+ * @typedef {{ tsv: System.UInt32 }} TrainerInformation
  */
 /**
  * いますぐバトル「さいきょう」にカーソルが合った状態から、seedを求めて返す。
@@ -70,25 +140,51 @@ function selectSeed(targetSeeds, controller, capture, sequences, tessConfig, det
  * @param {Sunameri.TesseractConfig} tessConfig 
  * @param {QuickBattleDetectInformation} detectInfo 
  * @param {TrainerInformation} trainerInfo 
- * @returns {any}
+ * @throws {Error} 5回連続でgetQuickBattleDataの取得に失敗した場合
+ * @returns {System.UInt32}
  */
-function getCurrentSeed(controller, capture, sequences, tessConfig, detectInfo, trainerInfo = {tsv: (0x10000).toUInt32()}) {
+function getCurrentSeed(controller, capture, sequences, tessConfig, detectInfo, trainerInfo) {
 
     /** @type {{ Count(): number }} */
     let result;
 
+    let count = 0;
+
     controller.run(sequences.loadParties);
 
     let mat = capture.getFrame();
-    let data1 = getQuickBattleData(mat, tessConfig, detectInfo);
-    mat.Dispose();
+    let data1
+    try {
+        data1 = getQuickBattleData(mat, tessConfig, detectInfo);
+    } catch (error) {
+        System.Console.Error.WriteLine(error.message);
+        count++;
 
+        data1 = undefined;
+    } finally {
+        mat.Dispose();
+    }
+    
     do {
         controller.run([].concat(sequences.discardParties, sequences.loadParties));
 
         mat = capture.getFrame();
-        let data2 = getQuickBattleData(mat, tessConfig, detectInfo);
-        mat.Dispose();
+        let data2;
+        try {
+            data2 = getQuickBattleData(mat, tessConfig, detectInfo);
+            count = 0;
+        } catch (error) {
+            // 5回連続で失敗したらthrow
+            System.Console.Error.WriteLine(error.message);
+            count++;
+            if (count === 5) {
+                throw new Error(`getQuickBattleData failed ${count} times in a row.`);
+            }
+
+            data2 = undefined;
+        } finally {
+            mat.Dispose();
+        }
 
         if (typeof data1 !== 'undefined' && typeof data2 !== 'undefined') {
             result = XDDatabase.SearchSeed(
@@ -103,15 +199,18 @@ function getCurrentSeed(controller, capture, sequences, tessConfig, detectInfo, 
 
     } while (true);
 
+    // なぜかnumberが返ってくるんだよな...
     return result[0].toUInt32();
 }
 
+/** @typedef {{ pIndex: number, eIndex: number, hp: number[] }} QuickBattleData */
 /**
  * 画像からQuickBattleDataを返す
  * 
  * @param {Sunameri.Mat} mat
  * @param {Sunameri.TesseractConfig} tessConfig
  * @param {QuickBattleDetectInformation} detectInfo
+ * @throws {Error} 不正な画像の場合・いますぐバトルが表示されていない場合 など
  * @returns {QuickBattleData}
  */
 function getQuickBattleData(mat, tessConfig, detectInfo) {
@@ -140,8 +239,7 @@ function getQuickBattleData(mat, tessConfig, detectInfo) {
     // mat_hp_3.SaveImage(`${fileName}-hp_3_${hp_3}.png`);
 
     if (pIndex === -1 || eIndex === -1) {
-        System.Console.WriteLine("getQuickBattleData failed");
-        return undefined; // 失敗したときにundefined返すってOKなのか...？
+        throw new Error("getQuickBattleData failed");
     }
 
     mat_pIndex.Dispose();
@@ -162,6 +260,72 @@ function getQuickBattleData(mat, tessConfig, detectInfo) {
     return result;
 }
 
+/**
+ * 
+ * @param {SelectedResult} selectedResult 
+ * @param {Sunameri.SerialPortWrapper} controller 
+ * @param {Sunameri.VideoCaptureWrapper} capture 
+ * @param {Sequences} sequences 
+ * @param {Sunameri.TesseractConfig} tessConfig 
+ * @param {QuickBattleDetectInformation} detectInfo 
+ * @param {TrainerInformation} trainerInfo 
+ * @param {WaitTimeInformation} waitTimeInfo  
+ */
+function adjustSeed(selectedResult, controller, capture, sequences, tessConfig, detectInfo, trainerInfo, waitTimeInfo) {
+
+    advanceByMoltres(selectedResult, controller, capture, sequences, tessConfig, detectInfo, waitTimeInfo);
+
+    /** @type {System.UInt32} */
+    const currentSeed = getCurrentSeed(controller, capture, sequences, tessConfig, detectInfo, trainerInfo);
+    
+    System.Console.WriteLine(JSON.stringify({currentSeed: currentSeed.value}));
+
+}
+
+function advanceByMoltres(selectedResult, controller, capture, sequences, tessConfig, detectInfo, waitTimeInfo) {
+
+    // ファイヤーが出るまで再生成
+    while (true) {
+        const mat = capture.getFrame();
+        let quickBattleData;
+        try {
+            quickBattleData = getQuickBattleData(mat, tessConfig, detectInfo)
+        } catch (error) {
+            System.Console.Error.WriteLine(error.message);
+            quickBattleData = undefined;
+        } finally {
+            mat.Dispose();
+        }
+
+        if (typeof quickBattleData !== 'undefined') {
+            if (quickBattleData.eIndex === 2) {
+                break;
+            }    
+        }
+
+        controller.run(
+            [].concat(
+                sequences.discardParties,
+                sequences.loadParties
+            )
+        );
+    }
+
+    // 戦闘入って出る
+    controller.run(sequences.entryToBattle);
+    const milliseconds = Math.floor(selectedResult.waitTime.Subtract(waitTimeInfo.minimum).TotalMilliseconds);
+    Sunameri.getTimer().sleep(milliseconds);
+
+    // 高速消費後1回分捨てる
+    controller.run(
+        [].concat(
+            sequences.exitBattle,
+            sequences.loadParties,
+            sequences.discardParties
+        )
+    );
+}
+
 (() => {
     System.Console.WriteLine(__dirname);
     const controller = Sunameri.getController({
@@ -178,8 +342,11 @@ function getQuickBattleData(mat, tessConfig, detectInfo) {
     timer.sleep(2000);
 
     const targetSeeds = [
-        0xbadface
-    ].toUInt32Array();
+        0x0,
+        0xbadface,
+        0xdeadbeef,
+        0xFFFFFFFF
+    ];
 
     /** @type {Sunameri.TesseractConfig} */
     const tessConfig = {
@@ -234,13 +401,45 @@ function getQuickBattleData(mat, tessConfig, detectInfo) {
         }
     }
 
+    /** @type {AdvanceInformation} */
+    const advanceInfo = {
+        /** 高速消費 */
+        faster: {
+            /**
+             * 消費速度 (消費/s)
+             * 
+             * 「いますぐバトル」でファイヤーを見る場合: 約3842消費/s
+             */
+            perSecond: 3842
+        }//,
+        /**
+         * ロード後の消費
+         * 
+         * ロードさせない場合は定義しない
+         */
+        // afterLoad: {
+        //     /** ロードにかかる消費 (オブジェクトの読み込みなど？) */
+        //     byLoading: number,
+        //     /** 「もちもの」消費 */
+        //     byOpeningItems: number
+        // }
+    }
+
+    /** @type {WaitTimeInformation} */
+    const waitTimeInfo = {
+        maximum: System.TimeSpan.Parse("0.03:00:00"),
+        minimum: System.TimeSpan.Parse("0.00:02:00")
+    }
+
     /** @type {TrainerInformation} */
     const trainerInfo = {
         tsv: (0x10000).toUInt32()
     }
 
     enableVibration(controller, sequences);
-    selectSeed(targetSeeds, controller, capture, sequences, tessConfig, detectInfo, trainerInfo);
+    const result = selectSeed(targetSeeds, controller, capture, sequences, tessConfig, detectInfo, trainerInfo, advanceInfo, waitTimeInfo);
+    System.Console.WriteLine(JSON.stringify({pivotSeed: result.pivotSeed.value, targetSeed: result.targetSeed.value, waitTime: result.waitTime.ToString()}));
+    adjustSeed(result, controller, capture, sequences, tessConfig, detectInfo, trainerInfo, waitTimeInfo);
 
     timer.sleep(2000);
     controller.Dispose();
